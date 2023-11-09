156fb2893092a2450417bc0b3f758cdfde778145
jungyu333 <jungyu3826@gmail.com>
2023-11-09 20:54:13 +0900

    [feat] add PasswordInput Component Doc

--- src/components/inputs/PasswordInput/PasswordInput.tsx
+++ src/components/inputs/PasswordInput/PasswordInput.tsx
@@ -2,13 +2,14 @@ import { useState } from 'react';
 import * as S from './passwordinput.styles';
 import { PasswordInputProps } from './PasswordInput.type';
 import { svgCloseEye, svgEye } from '../../../assets';
+import { UIProvider } from '../../general';
 
 export const PasswordInput = ({
   label,
   placeholder,
   register,
   error,
-  isLabel,
+  isLabel = true,
   width = '400px',
   height = '56px',
 }: PasswordInputProps) => {
@@ -16,30 +17,32 @@ export const PasswordInput = ({
   const [isShowPassWord, setIsShowPassWord] = useState(false);
 
   return (
-    <S.Input $width={width}>
-      <S.InnerContainer
-        $height={height}
-        $isFocused={isFocused}
-        $isError={error ? true : false}
-      >
-        {isLabel && <label>{label}</label>}
-        <div>
-          <input
-            onFocus={() => setIsFocused(true)}
-            {...register}
-            autoComplete="off"
-            onBlur={() => setIsFocused(false)}
-            type={isShowPassWord ? 'text' : 'password'}
-            placeholder={placeholder}
-          />
+    <UIProvider>
+      <S.Input $width={width}>
+        <S.InnerContainer
+          $height={height}
+          $isFocused={isFocused}
+          $isError={error ? true : false}
+        >
+          {isLabel && <label>{label}</label>}
+          <div>
+            <input
+              onFocus={() => setIsFocused(true)}
+              {...register}
+              autoComplete="off"
+              onBlur={() => setIsFocused(false)}
+              type={isShowPassWord ? 'text' : 'password'}
+              placeholder={placeholder}
+            />
 
-          <span onClick={() => setIsShowPassWord((prev) => !prev)}>
-            {!isShowPassWord ? svgCloseEye : svgEye}
-          </span>
-        </div>
-      </S.InnerContainer>
+            <span onClick={() => setIsShowPassWord((prev) => !prev)}>
+              {!isShowPassWord ? svgCloseEye : svgEye}
+            </span>
+          </div>
+        </S.InnerContainer>
 
-      {error && <S.Validation>{error}</S.Validation>}
-    </S.Input>
+        {error && <S.Validation>{error}</S.Validation>}
+      </S.Input>
+    </UIProvider>
   );
 };
