0666a6e7e91d28eb8476afd6345c31f5c2bb4149
jungyu333 <jungyu3826@gmail.com>
2023-11-21 20:40:58 +0900

    [feat] add LineText Component Doc

--- src/components/decorations/LineText/LineText.tsx
+++ src/components/decorations/LineText/LineText.tsx
@@ -1,12 +1,15 @@
+import { UIProvider } from '../../general';
 import { LineTextProps } from './LineText.type';
 import * as S from './lineText.styles';
 
 export const LineText = ({ label, width = '400px' }: LineTextProps) => {
   return (
-    <S.LineText $width={width}>
-      <div />
-      {label}
-      <div />
-    </S.LineText>
+    <UIProvider>
+      <S.LineText $width={width}>
+        <div />
+        {label}
+        <div />
+      </S.LineText>
+    </UIProvider>
   );
 };
